6683719ae1e1c80daf8833d5276c4e91a5b4d6fe
hobbes@theoti.me <Thotix>
2016-03-21 11:52:31 +0100

    Encapsulate the title var in my function

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
-var title = document.title;
-
 var DontForgetMe = (function () {
-    function init() {
-        _changeTitle();
-    }
 
-    function _changeTitle() {
-        document.addEventListener('visibilitychange', function () {
-            if (document.hidden) {
-                document.title = "Don't forget me !"
-            } else {
-                document.title = title
-            }
-        })
-    }
+	var title = document.title;
 
-    return {
-        init: init
-    }
+	function init() {
+		_changeTitle();
+	}
+
+	function _changeTitle() {
+		document.addEventListener('visibilitychange', function () {
+			if (document.hidden) {
+				document.title = "Don't forget me !"
+			} else {
+				document.title = title
+			}
+		})
+	}
+
+	return {
+		init: init
+	}
 })()
 DontForgetMe.init();
